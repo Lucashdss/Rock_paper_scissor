378ba3f5385da629027c4eb2030703559c77c220
LucasHS <159789872+LucasHSKKK@users.noreply.github.com>
2025-11-26 16:21:49 +0100

    refactor the drawing in the while loop to functions

--- src/animation/animation.cpp
+++ src/animation/animation.cpp
@@ -9,7 +9,7 @@ Animation::Animation(){
     PaperGifImage = LoadImageAnim("assets/paper.gif", &AnimationFrame);
     ScissorsGifImage = LoadImageAnim("assets/scissors.gif", &AnimationFrame);
     sourceRec = { 0.0f, 0.0f, (float)RockGifImage.width*(-1), (float)RockGifImage.height};
-    destRec = { 0.0f, 100.0f, (float)RockGifImage.width, (float)RockGifImage.height};
+    destRec = { 0.0f, 200.0f, (float)RockGifImage.width, (float)RockGifImage.height};
 }
 
 Animation::~Animation() {
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 void updateCountDownText();
 void MakeThreadSleepAndCountDown();
 void DrawCountDownText();
+void drawPlayerMoveAnimation(int &playerMoveIndex, Animation& HandSigns);
+void drawEnemyMoveAnimation(int &EnemyMoveIndex, AnimationEnemy& HandSignsEnemy);
+int CheckWhoWins(int &playerMoveIndex, int &EnemyMoveIndex);
 
 int *countDown = new int(4);
 std::string CountDownText;
@@ -22,8 +25,13 @@ int main()
     std::unique_ptr<Animation> HandSigns = std::make_unique<Animation>();
     std::unique_ptr<AnimationEnemy> HandSignsEnemy = std::make_unique<AnimationEnemy>();
     std::unique_ptr<player> Player = std::make_unique<player>();
+    std::unique_ptr<cpuEnemy> CpuEnemy = std::make_unique<cpuEnemy>();
 
     Player->playerInput();
+    int playerMoveIndex = Player->getMoveIndex();
+    int EnemyMoveIndex = CpuEnemy->generateRandomMoveIndex();
+    
+    CheckWhoWins(playerMoveIndex, EnemyMoveIndex);
 
     while (!WindowShouldClose())
     {
@@ -36,14 +44,9 @@ int main()
 
         DrawCountDownText();
 
-        if (countDown == nullptr){
-            DrawTexturePro(HandSigns->DrawPaperAnimation(),
-                        HandSigns->sourceRec,
-                        HandSigns->destRec,
-                        {0,0},
-                        0.f,
-                        WHITE);
-            DrawTexture(HandSignsEnemy->DrawScissorsAnimation(), 200, 100, WHITE);
+        if(countDown == nullptr){
+            drawPlayerMoveAnimation(playerMoveIndex, *HandSigns);
+            drawEnemyMoveAnimation(EnemyMoveIndex, *HandSignsEnemy);
         }
 
         EndDrawing();
@@ -85,4 +88,59 @@ void DrawCountDownText(){
         DrawText("Go!", (800 - textWidth) / 2, (600 - fontSizeCountdown) / 2, fontSizeCountdown, WHITE);
         countDown = nullptr;
     }
+};
+
+void drawPlayerMoveAnimation(int &playerMoveIndex, Animation& HandSigns){
+    if (playerMoveIndex == 1){
+        DrawTexturePro(HandSigns.DrawRockAnimation(),
+                    HandSigns.sourceRec,
+                    HandSigns.destRec,
+                    {0,0},
+                    0.f,
+                    WHITE);
+    }
+    else if (playerMoveIndex == 2){
+        DrawTexturePro(HandSigns.DrawPaperAnimation(),
+                    HandSigns.sourceRec,
+                    HandSigns.destRec,
+                    {0,0},
+                    0.f,
+                    WHITE);
+    }
+    else if (playerMoveIndex == 3){
+        DrawTexturePro(HandSigns.DrawScissorsAnimation(),
+                    HandSigns.sourceRec,
+                    HandSigns.destRec,
+                    {0,0},
+                    0.f,
+                    WHITE);
+    }
+};
+
+void drawEnemyMoveAnimation(int &EnemyMoveIndex, AnimationEnemy& HandSignsEnemy){
+    int positionX = 200;
+    int positionY = 200;
+    
+    if (EnemyMoveIndex == 1){
+        DrawTexture(HandSignsEnemy.DrawRockAnimation(), positionX, positionY, WHITE);
+    }
+    else if (EnemyMoveIndex == 2){
+        DrawTexture(HandSignsEnemy.DrawPaperAnimation(), positionX, positionY, WHITE);
+    }
+    else if (EnemyMoveIndex == 3){
+        DrawTexture(HandSignsEnemy.DrawScissorsAnimation(), positionX, positionY, WHITE);
+    }
+};
+
+int CheckWhoWins(int &player1, int &player2){
+    int result = (3 + player1 - player2) % 3;
+
+    if (result == 0)
+        std::cout << "Draw!\n";
+    else if (result == 1)
+        std::cout << "Player 1 wins!\n";
+    else
+        std::cout << "Player 2 wins!\n";
+
+    return result;
 };
